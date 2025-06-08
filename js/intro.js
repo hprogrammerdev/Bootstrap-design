fcdb1642d72d8763893d5078dffd589827da7794
unknown <hprogrammer.dev@gmail.com>
2025-06-08 15:08:13 +0300

    Add Files

---
+++ js/intro.js
@@ -0,0 +1,33 @@
+// Bootstrap
+// vid 3
+// 
+/*
+- p-lg-3
+اي في الشاشات الكبيرة
+- sticky-top
+يثبت الناف في اعلى الشاشة
+=====
+
+fs-5 => h5 size
+fs-6 => h6 size
+
+===================
+text-white-50 => #eee
+text-black-50 => gray
+===
+text-uppercase تكبير النص
+
+lh-l => line-height: 2
+lh-base => line-height: 1.5
+
+========
+fa-nx مضاعفة حجم الايقونة من
+font-awesome
+
+======
+bg-white => background white
+img-fluid كي لا تخرج الصورة من الحيز التي هي فيه
+
+text-md-start => text-align: left
+
+*/
